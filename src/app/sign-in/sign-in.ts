58f2ca8dbdee581d16bcdfd04b410bee20f71976
Tarun K. Gupta <tg.github@gmail.com>
2025-09-14 01:41:04 +0530

    otp enter screen done

--- src/app/sign-in/sign-in.ts
+++ src/app/sign-in/sign-in.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-sign-in',
   imports: [],
   templateUrl: './sign-in.html',
-  styleUrl: './sign-in.css'
+  styleUrl: './sign-in.css',
 })
 export class SignIn {
+  emailEntered: boolean = false;
+  intervalId: any;
+  time = signal(10);
+  formattedTime = signal('');
+  resentOTP: boolean = false;
 
+  constructor() {
+    effect(() => {
+      this.formattedTime.set(this.transform(this.time()));
+    });
+  }
+
+  sendOTP() {
+    this.emailEntered = true;
+    this.startTimer();
+  }
+
+  startTimer() {
+    this.intervalId = setInterval(() => {
+      if (this.time() > 0) {
+        this.time.update((v) => v - 1);
+      } else {
+        this.clearTimer();
+      }
+    }, 1000);
+  }
+
+  transform(seconds: number): string {
+    const m = Math.floor(seconds / 60);
+    const s = seconds % 60;
+    return `${m}:${s < 10 ? '0' : ''}${s}`;
+  }
+
+  clearTimer() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  }
+
+  ngOnDestroy() {
+    this.clearTimer();
+  }
 }
